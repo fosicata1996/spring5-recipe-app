d173f8fa120126e80552c246ec8da75b62eb7a03
catalin.fosalau <fosalaucatalin@gmail.com>
2024-02-28 14:15:29 +0200

    Add Ingredient POJO

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,24 @@
+package guru.springframework.domain;
+
+import java.math.BigDecimal;
+
+public class Ingredient {
+    private String description;
+    private BigDecimal amount;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
