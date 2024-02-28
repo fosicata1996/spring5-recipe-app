a6f0d748123b1e2f8c56b2010ac90c9b9f0c5160
catalin.fosalau <fosalaucatalin@gmail.com>
2024-02-28 14:24:08 +0200

    Add UnitOfMeasure POJO

---
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -0,0 +1,13 @@
+package guru.springframework.domain;
+
+public class UnitOfMeasure {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
