272e017a7435cc76c355c530cfb473096e6b61df
catalin.fosalau <fosalaucatalin@gmail.com>
2024-02-28 14:34:58 +0200

    Add Category POJO

---
+++ src/main/java/guru/springframework/domain/Category.java
@@ -0,0 +1,24 @@
+package guru.springframework.domain;
+
+import java.util.Set;
+
+public class Category {
+    private String description;
+    private Set<Recipe> recipes;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
